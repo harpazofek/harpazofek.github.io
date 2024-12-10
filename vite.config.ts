0d2d2fb0b71dbd4d4552245f83effcab694c09d7
harpazofek <oharpaz1@gmail.com>
2024-12-10 12:50:41 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 
 // Set the base path for GitHub Pages deployment
 export default defineConfig({
-  base: '/', // GitHub Pages serves files at the root for harpazofek.github.io
+  base: '/harpazofe.github.io', // GitHub Pages serves files at the root for harpazofek.github.io
   plugins: [react()],
 });
